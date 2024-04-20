6efc610e4a30de23df50bad88fb1140da6d8135f
Marcigdd <marcigdd@gmail.com>
2024-04-20 13:32:08 +0200

    chore: update apipath url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://ehwiblt3kk.execute-api.us-east-2.amazonaws.com/prod",
+  bff: "https://hynb9z8u91.execute-api.us-east-2.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
